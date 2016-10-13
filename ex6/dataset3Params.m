2ec30690a05bd4e4519bc44d44befc27f4a9b674
klausrz <ndhdang@gmail.com>
2016-10-13 10:31:38 +0700

    add ex6

---
+++ ex6/dataset3Params.m
@@ -0,0 +1,66 @@
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%EX6PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   sigma. You should complete this function to return the optimal C and 
+%   sigma based on a cross-validation set.
+%
+
+% You need to return the following variables correctly.
+C = 1;
+sigma = 0.3;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example, 
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using 
+%        mean(double(predictions ~= yval))
+%
+
+val = [0.01 0.03 0.1 0.3 1 3 10 30]';
+for i = 1:length(val)
+	cC = val(i);
+	for j = 1:length(val)				
+		csigma = val(j);
+		
+		% debug messages %
+		fprintf(['%d - Training with C = %0.5f, sigma = %0.5f\n\n'], (i - 1) * length(val) +  j, cC, csigma);		
+		fprintf(['\nCurrent C: %0.5f'], C );
+		fprintf(['\nCurrent sigma: %0.5f\n'], sigma );
+		
+		model = svmTrain(X, y, cC, @(x1, x2) gaussianKernel(x1, x2, csigma));
+		pred = svmPredict(model, Xval);
+		error = mean(double(pred ~= yval))
+		
+		if (exist("err", "var") != 1)
+			err = error;		
+			C = cC;
+			sigma = csigma;
+			fprintf(['Set FIRST err: %0.5f\n'], err );			
+		else
+			if(error < err)
+				fprintf(['Setting err = : %0.5f\n'], error );
+				err = error;
+				C = cC;
+				sigma = csigma;
+			end
+		end
+		fprintf(['\nCurrent err: %0.5f\n'], err );
+	end
+end
+
+
+
+
+
+
+
+% =========================================================================
+
+end
